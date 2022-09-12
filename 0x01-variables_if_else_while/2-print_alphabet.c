46958b7a788d7ed7dbf69b67a9b409ed5ee72d00
Meazi <meazimekonnen93@gmail.com>
2022-09-12 11:26:09 -0700

    Print lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Successi)
+*/
+int main(void)
+{
+int ch='a';
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			putchar('\n');
+			ch++;
+		}
+return (0);
+}
