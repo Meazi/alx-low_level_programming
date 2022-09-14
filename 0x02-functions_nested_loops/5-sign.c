ef9e73a0c87448169e07ff1be3881537224baaa2
Meazi <meazimekonnen93@gmail.com>
2022-09-14 12:05:01 -0700

    print sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,21 +11,21 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		return (1);
 		_putchar('+');
 		_putchar('\n');
-		return (1);
 	}
 	else if (n == 0)
 	{
+		return (0);
 		_putchar('0');
 		_putchar('\n');
-		return (0);
 	}
 	else
 	{
+		return (-1);
 		_putchar('-');
 		_putchar('\n');
-		return (-1);
 	}
 	return (0);
 }
