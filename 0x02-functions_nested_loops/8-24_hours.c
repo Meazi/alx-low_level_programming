fa7c93bff46eb634357ede30fb00a694dfc5418e
Meazi <meazimekonnen93@gmail.com>
2022-09-14 14:35:10 -0700

    print minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute of the day
+ * void: nothing to return
+ * Return:
+ */
+void jack_bauer(void)
+{
+	int h, hh;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (hh = 0; hh <= 59; hh++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(hh / 10 + '0');
+			_putchar(hh % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
