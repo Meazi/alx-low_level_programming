0b9b853767d97b428efef675e40f33af0a517cbc
Meazi <meazimekonnen93@gmail.com>
2022-09-16 11:22:02 -0700

    confused

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,15 +10,7 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	} else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	} else
-		printf("%d is Zero\n", 0);
+	n = 0;
+	positive_or_negative(i);
 	return (0);
 }
