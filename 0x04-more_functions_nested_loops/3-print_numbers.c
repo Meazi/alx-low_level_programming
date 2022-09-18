0987caa17b65f57fd600cb4bc3cc30c4c4846d55
Meazi <meazimekonnen93@gmail.com>
2022-09-18 13:02:53 -0700

    two and three together

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints from 0 to 9
+ * void: nothing to return
+ * Return:
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar( i ); 
+	}
+		
+		_putchar('\n');
+}
