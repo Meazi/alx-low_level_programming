5e3ab54d4193554fd51a01f9511515f693a32c53
Meazi <meazimekonnen93@gmail.com>
2022-09-19 13:16:51 -0700

    swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of two integers
+ * @a: parameter passed from main
+ * @b: parameter passed from main
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
