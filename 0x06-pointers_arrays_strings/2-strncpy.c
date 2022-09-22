6c9716aac652167981bb65c6def9afaf49ecd6a3
Meazi <meazimekonnen93@gmail.com>
2022-09-22 14:13:18 -0700

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - copies one string onto another
+ * @dest: destination
+ * @src: source
+ * @n: max input passed from main
+ * Return: char value
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		if (i < n)
+			dest[i] = src[i];
+	while (i < n)
+		dest[i++] = '\0';
+	return (dest);
+}
