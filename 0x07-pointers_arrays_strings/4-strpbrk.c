b113aa182bf5909afbabec4a3b98da4cf9f177dc
Meazi <meazimekonnen93@gmail.com>
2022-09-27 13:25:43 -0700

    search for any set of string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - search a string for any set of bytes
+ * @s: char variable
+ * @accept: original string
+ * Return: pointer to a string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (s);
+}
