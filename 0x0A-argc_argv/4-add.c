42e9d43771c8d5195586adc016381cd1c8ba630a
Meazi <meazimekonnen93@gmail.com>
2022-10-05 10:10:13 -0700

    add all the numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prit the product of numbers
+ * @argc: argument counter
+ * @argv: array of argc
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, addition = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		else
+		addition = addition + (atoi(argv[i]));
+	}
+	printf("%d\n", addition);
+	return (0);
+}
