0bf02f90e55b46108ba4c91eda7e219382b81f76
Meazi <meazimekonnen93@gmail.com>
2022-10-05 06:39:38 -0700

    print the multiplication of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 /**
  * main - print multiple of two numbers
  * @argc: argument counter
@@ -10,12 +10,15 @@ int main(int argc, char *argv[])
 {
 	int result;
 
+	if (argv == NULL)
+	{
+		return (0);
+	}
 	if (argc == 1 || argc > 3)
 	{
 		printf("%s\n", "Error");
 		return (0);
 	}
-	
 	result = (atoi(argv[1])) * (atoi(argv[2]));
 	printf("%d\n", result);
 
