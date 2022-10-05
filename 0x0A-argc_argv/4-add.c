a71777dc31a4ce0d09cae31141063792836997cd
Meazi <meazimekonnen93@gmail.com>
2022-10-05 10:31:58 -0700

    add all the given numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,13 +10,12 @@ int main(int argc, char *argv[])
 {
 	int i, addition = 0;
 
-	if (argc == 1)
+	if (argc <= 1)
 	{
 		printf("%d\n", 0);
-	}
-	if (argc < 1)
 		return (0);
-	for (i = 1; i < argc; i++)
+	}
+	for (i = 2; i < argc; i++)
 	{
 		if (!atoi(argv[i]))
 		{
