204d1746e87ffe516b48fe8f6b074e765ea10669
Meazi <meazimekonnen93@gmail.com>
2022-10-05 06:36:17 -0700

    prit multiplication of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h> 
+/**
+ * main - print multiple of two numbers
+ * @argc: argument counter
+ * @argv: array of argc
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc == 1 || argc > 3)
+	{
+		printf("%s\n", "Error");
+		return (0);
+	}
+	
+	result = (atoi(argv[1])) * (atoi(argv[2]));
+	printf("%d\n", result);
+
+	return (0);
+}
