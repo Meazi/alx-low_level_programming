ab46ad856ae949d7368931ffddd73f5f16fe87d8
Meazi <meazimekonnen93@gmail.com>
2022-10-10 07:42:55 -0700

    structure initialization

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - structure initialser
+ * @d: structure pointer
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	dog *d;
+
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+}
