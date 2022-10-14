d5f71a65c3d1931376d8547bfbe81c8e29e6f842
Meazi <meazimekonnen93@gmail.com>
2022-10-14 05:10:59 -0700

    print array elements

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
 /**
  * array_iterator - print array elements
+ * @action: pointer to function print_elem and print_elem_hex
  * @array: parameter passed
  * @size: size of an array
  * Return: nothing
@@ -9,6 +10,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
+	if (array == NULL || size == 0 || action == 0)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
