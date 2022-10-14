08b8e331ce71744fdf8da2a9012837a0da4e2fdc
Meazi <meazimekonnen93@gmail.com>
2022-10-14 06:00:28 -0700

    search for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integr
+ * @array: parameter passed
+ * @size: size of the array
+ * @cmp: pointer to is_98, is_strictly_positive and abs_is functions
+ * Return: index of the array
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return(-1);
+
+	for(i = 0; i < size; i++)
+	{
+	 	if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
