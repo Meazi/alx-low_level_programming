06a10f0d590d52e05eb28fe7d872db5ce43d4fc3
Meazi <meazimekonnen93@gmail.com>
2022-10-25 13:24:22 -0700

    return a node at a specified index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,23 +3,22 @@
 /**
  * get_nodeint_at_index - a function that returns a node at a specified index
  * @head: pointer to the first node of a list
+ * @index: place a specific node is located
  * Return: node at the specified index
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
+	listint_t *temp = head;
 
-	while (count < index)
+	while (temp && (count < index))
 	{
-		if (head->next == NULL)
-		{
-			return (NULL);
-		}
-		else
-		{
-			count++;
-			head = head->next;
-		}
+		temp = temp->next;
+		counter++;
 	}
-	return (head);
+
+	if (counter < index)
+		return (NULL);
+
+	return (temp);
 }
