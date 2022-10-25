cc2fc42746ec631fe1f92a84f5e14c996e50dac0
Meazi <meazimekonnen93@gmail.com>
2022-10-25 07:01:01 -0700

    delete all the nodes and set head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - delets all the nodes and set head to NULL
+ * @head: pointer to the first node of a list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *ptr = NULL;
+	tmp = *head;
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		free(tmp);
+		tmp = ptr;
+	}
+	*head = NULL;
+}
