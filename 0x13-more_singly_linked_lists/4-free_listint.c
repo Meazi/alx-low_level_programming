8046e7637867cc02ea38d3637843c9f9a158a704
Meazi <meazimekonnen93@gmail.com>
2022-10-25 06:41:36 -0700

    delete a node one by one

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * free_listint - deletes all the nodes one by one 
+ * free_listint - deletes all the nodes one by one
  * @head: pointer to the first node
  * Return : nothing
  */
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
+
 	tmp = head;
 
 	while (tmp->next != NULL)
