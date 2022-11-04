b83349576e4c00774cf4304762585b4ee0b03573
Meazi <meazimekonnen93@gmail.com>
2022-11-04 10:40:15 -0700

    append text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,11 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * _strlen - a function to find string length
  * @s: pointer to character
